b77b6595ce6085e37ccc14ca8d2eb5d6cdf67313
ocpu <martin.hovre@railit.se>
2021-05-20 15:51:21 +0200

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,19 @@
+# Typed Flags for Deno
+
+This is a simple module to to declaratively set the type of the flags you accept. It uses the std/flags under the hood so parsing of flags is handled by it and type ensuring is provided by this module. All parts of the flag definition are strictly type defined (you event get some auto completion at places).
+
+```typescript
+const { _: args, ...flags } = parseFlags({
+  help: Boolean, // Use either of Boolean, String, or Number constructors to define your type
+  port: { // Make the definition an object if you want to specify more than type
+    type: Number,
+    default: 3000, // Default values must be in the type you specify
+    alias: 'p', // Define an alias or an array of them
+  },
+  name: String,
+  h: 'help', // Another way of specifying an alias to a command (these can be auto completed)
+}/*, <array of arguments default Deno.args>*/)
+
+type MyFlags = typeof flags // { help: boolean, post: number, name: string | undefined }
+type MyArgs = typeof args // string[]
+```
---
+++ mod.ts
@@ -0,0 +1,103 @@
+import { parse } from "https://deno.land/std@0.90.0/flags/mod.ts";
+
+interface FlagDefinitionObject<T, TCtor extends LiteralConstructor<T>> {
+  type: TCtor;
+  alias?: string | string[];
+  default?: T;
+}
+
+type FlagDefinition =
+  | BooleanConstructor
+  | StringConstructor
+  | NumberConstructor
+  | FlagDefinitionObject<boolean, BooleanConstructor>
+  | FlagDefinitionObject<string, StringConstructor>
+  | FlagDefinitionObject<number, NumberConstructor>
+
+type ToLiteral<L> = L extends String ? string
+  : L extends Boolean ? boolean
+  : L extends Number ? number
+  : L extends string ? string
+  : L extends boolean ? boolean
+  : L extends number ? number
+  : never
+
+type FromLiteral<L> = L extends String ? String
+  : L extends Boolean ? Boolean
+  : L extends Number ? Number
+  : L extends string ? String
+  : L extends boolean ? Boolean
+  : L extends number ? Number
+  : never
+
+type LiteralConstructor<T> = new (...args: any[]) => FromLiteral<T>
+type FlagType<FlagDef extends FlagDefinition> = FlagDef extends LiteralConstructor<infer R>
+  ? ToLiteral<R> extends boolean ? boolean : ToLiteral<R> | undefined
+  : FlagDef extends FlagDefinitionObject<any, any>
+    ? FlagDef["type"] extends new () => infer TR
+      ? FlagDef["default"] extends ToLiteral<TR> ? ToLiteral<TR>
+      : ToLiteral<TR> extends boolean ? ToLiteral<TR>
+      : ToLiteral<TR> | undefined
+    : never
+  : never;
+type OnlyKeysOfType<T, IncludeType> = { [K in keyof T]: T[K] extends IncludeType ? never : K }[keyof T]
+type OmitKeysOfType<T, ExcludeType> = { [K in keyof T]: T[K] extends ExcludeType ? K : never }[keyof T]
+type FlagResult<Def extends FlagsDefinition<Def>> = {
+  [K in keyof Omit<Def, OmitKeysOfType<Def, string>>]: Def[K] extends FlagDefinition ? FlagType<Def[K]> : never
+}
+
+export interface FlagsDefinition<Def extends FlagsDefinition<Def>> {
+  [name: string]: FlagDefinition | OnlyKeysOfType<Def, string>
+}
+
+export type FlagsResult<Def extends FlagsDefinition<Def>> = FlagResult<Def> & { _: readonly string[] }
+
+export function parseFlags<Def extends FlagsDefinition<Def>>(def: Def): FlagsResult<Def>
+export function parseFlags<Def extends FlagsDefinition<Def>>(def: Def, args: string[]): FlagsResult<Def>
+export function parseFlags<Def extends FlagsDefinition<Def>>(def: Def, args: string[] = Deno.args): FlagsResult<Def> {
+  const entries = Object.entries(def) as [string, FlagDefinition | OnlyKeysOfType<Def, string>][]
+  const alias = (entries
+    .filter(([, value]) => typeof value === 'string') as [string, string][])
+    .reduce((aliases, [alias, flag]) => {
+      if (flag in aliases) aliases[flag].includes(alias) || aliases[flag].push(alias)
+      else aliases[flag] = [alias]
+      return aliases
+    }, {} as Record<string, string[]>)
+
+  const defaults = {} as Record<string, string | number | boolean>
+  const strings = [] as string[]
+  const booleans = [] as string[]
+  for (const [key, value] of entries) {
+    if (typeof value === 'string') continue
+    if (value === Boolean) booleans.push(key)
+    else if (value === String || value === Number) strings.push(key)
+    else if (typeof value === 'object') {
+      if (value.type === Boolean) booleans.push(key)
+      else if (value.type === String || value.type === Number) strings.push(key)
+      if ('default' in value && value.default !== undefined) defaults[key] = value.default
+      if ('alias' in value) {
+        const current = alias[key] ??= []
+        if (Array.isArray(value.alias)) current.push(...value.alias.filter(alias => !current.includes(alias)))
+        else if (typeof value.alias === 'string' && current.includes(value.alias)) current.push(value.alias)
+      }
+    }
+  }
+
+  const result = parse(args, { alias, default: defaults, string: strings, boolean: booleans })
+
+  const res: { [key: string]: any } & { _: readonly string[] } = { _: result._.map((it) => "" + it) }
+
+  for (const [key, value] of entries) {
+    if (typeof value === 'string') continue
+    if (value === Boolean) continue
+    else if (value === String) res[key] = ''+result[key]
+    else if (value === Number) res[key] = +result[key]
+    else if (typeof value === 'object') {
+      if (value.type === Boolean) continue
+      else if (value.type === String) res[key] = ''+result[key]
+      else if (value.type === Number) res[key] = +result[key]
+    }
+  }
+
+  return res as FlagResult<Def> & { _: readonly string[] };
+}
